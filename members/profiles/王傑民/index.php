cd75772ba7fa0ef2eaff486b080a8406e68b2a5e
Hank (X1 Carbon 8th) <wu.yusung@gmail.com>
2022-03-08 21:24:01 +0800

    x

--- members/profiles/王傑民/index.php
+++ members/profiles/王傑民/index.php
@@ -6,7 +6,7 @@
 <h5 class="media-heading">王傑民</h5>
 <small>2014&mdash;2016<br/>
 Master<br/>
-Now with Synology (群暉科技)</small></br>
+Now with Google</small></br>
 <ul class="pagination pagination-sm margin-v4px">
 <li><a href="mailto:chiehmin18@gmail.com"><span class="fa fa-envelope-o"></span></a></li>
 <li><a href="https://www.facebook.com/fatminmin" target="_blank"><span class="fa fa-facebook-official"></span></a></li>
--- members/profiles/盛宇航/index.php
+++ members/profiles/盛宇航/index.php
@@ -4,8 +4,8 @@
 <div class="media-body">
 <!--<h5 class="media-heading">English Name</h5>-->
 <h5 class="media-heading">盛宇航</h5>
-<small>2018&mdash;<br/>
-Master</small></br>
+<small>2018&mdash;2020, M.S.<br/>
+<small>Now with Google</small></br>
 <ul class="pagination pagination-sm margin-v4px">
 <li><a href="mailto:joy09272002@gmail.com"><span class="fa fa-envelope-o"></span></a></li>
 <li><a href="https://www.facebook.com/profile.php?id=100000371113720" target="_blank"><span class="fa fa-facebook-official"></span></a></li>
