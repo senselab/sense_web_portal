9b3bde7c9df703f04ba8c2f9afb96c4ddee2af36
Jun-Yu, Lai 賴俊宇 <108275000+aru311551131@users.noreply.github.com>
2024-05-15 16:52:20 +0800

    x

--- members/deprecate_profiles/吳育松/index.php
+++ members/deprecate_profiles/吳育松/index.php
@@ -7,8 +7,8 @@
 
 <ul class="pagination pagination-sm margin-v4px">
 <li><span class="fa fa-phone" data-toggle="tooltip" data-placement="bottom" title="+886-3-5712121 ext. 56623"></span></li>
-<li><a href="mailto:ysw@cs.nctu.edu.tw"><span class="fa fa-envelope-o"></span></a></li>
-<li><a href="https://sense.cs.nctu.edu.tw/members/profiles/%E5%90%B3%E8%82%B2%E6%9D%BE/nctu_cs_web/index.html" target="_blank"><span class="fa fa-link"></span></a></li>
+<li><a href="mailto:ysw@cs.nycu.edu.tw"><span class="fa fa-envelope-o"></span></a></li>
+<li><a href="https://sense.cs.nycu.edu.tw/members/profiles/%E5%90%B3%E8%82%B2%E6%9D%BE/nctu_cs_web/index.html" target="_blank"><span class="fa fa-link"></span></a></li>
 </ul>
 
 </div>
